b016253e85232bf61bfb9de26bef1b86b48b43ec
kmbarnett <kmbarnett94@gmail.com>
2020-06-15 09:47:55 -0600

    Updates

---
+++ precache-manifest.687ea98187f7b95ccac5f367fe1f8ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d5149976df1046789d80fbc37f989b",
+    "url": "/MintBean_0615/index.html"
+  },
+  {
+    "revision": "ee024c88adcb5f8a4a14",
+    "url": "/MintBean_0615/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfd7b315f90c92f677c7",
+    "url": "/MintBean_0615/static/js/2.f4de6fd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MintBean_0615/static/js/2.f4de6fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee024c88adcb5f8a4a14",
+    "url": "/MintBean_0615/static/js/main.47082270.chunk.js"
+  },
+  {
+    "revision": "76cf7defac5ce5d0f2a1",
+    "url": "/MintBean_0615/static/js/runtime-main.d9f6473d.js"
+  }
+]);
