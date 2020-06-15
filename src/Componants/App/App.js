dc040abbb06968ccf5db99c93e5dd2a30c375fec
kmbarnett <kmbarnett94@gmail.com>
2020-06-15 11:48:05 -0600

    Add set up

--- src/Componants/App/App.js
+++ src/Componants/App/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import mapboxgl from 'mapbox-gl'
 
 import './App.css';
 
+console.log(process.env.REACT_APP_MAPBOX_ACCESS_TOKEN);
+// mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 function App() {
+  let mapContainer
+  // useEffect(() => {
+  //   const map = new mapboxgl.Map({
+  //     container: mapContainer,
+  //     style: 'mapbox://styles/kmbarnett/ckbgqnk350rfz1ipidif98e98/draft',
+  //     center: [-77.04, 38.907],
+  //     zoom: 14
+  //   });
+  // },[])
+
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <h1>MintBean_0615</h1>
       </header>
-    </div>
+      <div ref={el => mapContainer = el} id='map'>
+      </div>
+    </main>
   );
 }
 
